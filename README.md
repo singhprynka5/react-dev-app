215a50cde0468395dd33ce966321bc780a8da2a5
Priyanka <singhprynka5@gmail.com>
2024-01-13 23:31:54 +0530

    react-testing-library - unit test cases

---
+++ README.md
@@ -0,0 +1,19 @@
+# Setting up Testing Library
+
+- Install React Testing Library
+- Install jest
+- Install Babel dependencies
+- Configure Babel
+- Configure Parcel Config file to disable default babel transpilation
+- Jest Configuration
+  - npx jest --init
+- Install jsdom library
+  - if using Jest 28 or later, jest-environment-jsdom package now must be installed separately.
+- Install @babel/preset-react
+  - to make JSX work in test cases
+- Include @babel/preset-react inside babel config
+- Install @testing-library/jest-dom
+
+# Run test cases
+
+- npm run test
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+test("Should load contact us component", () => {
+  render(<Contact />);
+  const heading = screen.getByRole("heading");
+  expect(heading).toBeInTheDocument();
+});
+
+test("Should load button inside contact us component", () => {
+  render(<Contact />);
+  const button = screen.getByRole("button");
+  const buttonByText = screen.getByText("Submit");
+  expect(button).toBeInTheDocument();
+  expect(buttonByText).toBeInTheDocument();
+});
